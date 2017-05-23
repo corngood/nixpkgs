3eac594e9790ba4f32539fb7f2a342f50a0fa7d1
David McFarland <corngood@gmail.com>
2017-05-23 10:36:22 -0300

    perl-LocaleGettext: fix test failure on cygwin by setting $LANG

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -7632,7 +7632,7 @@ let self = _self // overrides; _self = with self; {
     };
   };
 
-  LocaleGettext = buildPerlPackage {
+  LocaleGettext = buildPerlPackage ({
     name = "LocaleGettext-1.05";
     buildInputs = stdenv.lib.optional (stdenv.isFreeBSD || stdenv.isDarwin || stdenv.isCygwin) pkgs.gettext;
     NIX_CFLAGS_LINK = stdenv.lib.optional (stdenv.isFreeBSD || stdenv.isDarwin || stdenv.isCygwin) "-lintl";
@@ -7640,7 +7640,9 @@ let self = _self // overrides; _self = with self; {
       url = mirror://cpan/authors/id/P/PV/PVANDRY/gettext-1.05.tar.gz;
       sha256 = "15262a00vx714szpx8p2z52wxkz46xp7acl72znwjydyq4ypydi7";
     };
-  };
+  } // stdenv.lib.optionalAttrs stdenv.isCygwin {
+    LANG="C";
+  });
 
   LocaleMaketext = buildPerlPackage {
     name = "Locale-Maketext-1.23";
