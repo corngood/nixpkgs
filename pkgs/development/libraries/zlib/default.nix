4f8a76312f294d872153d16238fc59c9e3905db8
David McFarland <corngood@gmail.com>
2017-05-23 10:36:56 -0300

    zlib: fix cygwin build

--- pkgs/development/libraries/zlib/default.nix
+++ pkgs/development/libraries/zlib/default.nix
@@ -13,6 +13,8 @@ stdenv.mkDerivation rec {
     sha256 = "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1";
   };
 
+  patches = stdenv.lib.optional stdenv.isCygwin ./disable-cygwin-widechar.patch;
+
   postPatch = stdenv.lib.optionalString stdenv.isDarwin ''
     substituteInPlace configure \
       --replace '/usr/bin/libtool' 'ar' \
