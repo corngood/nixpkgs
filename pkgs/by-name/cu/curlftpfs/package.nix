daed1b2e54726c31a71b1d493d7205ed12212f63
David McFarland <corngood@gmail.com>
2019-09-25 23:59:42 -0300

    curlftpfs: add mount.fuse.curlftpfs symlink

--- pkgs/by-name/cu/curlftpfs/package.nix
+++ pkgs/by-name/cu/curlftpfs/package.nix
@@ -51,6 +51,10 @@ stdenv.mkDerivation rec {
 
   doCheck = false; # fails, doesn't work well too, btw
 
+  postInstall = ''
+    ln -s $out/bin/curlftpfs $out/bin/mount.fuse.curlftpfs
+  '';
+
   meta = with lib; {
     description = "Filesystem for accessing FTP hosts based on FUSE and libcurl";
     mainProgram = "curlftpfs";
