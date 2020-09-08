1bcb5cb8834fba6d6b6973b74a302c6d15d3bb2d
David McFarland <corngood@gmail.com>
2020-09-08 13:12:29 -0300

    libev: fix link error on cygwin

--- pkgs/development/libraries/libev/default.nix
+++ pkgs/development/libraries/libev/default.nix
@@ -9,6 +9,10 @@ stdenv.mkDerivation rec {
     sha256 = "0kil23cgsp0r5shvnwwbsy7fzxb62sxqzqbkbkfp5w54ipy2cm9d";
   };
 
+  postPatch = stdenv.lib.optionalString stdenv.hostPlatform.isCygwin ''
+    sed -i -e "s/libev_la_LDFLAGS =.*/\\0 -no-undefined/" Makefile.in
+  '';
+
   meta = {
     description = "A high-performance event loop/event model with lots of features";
     maintainers = [ stdenv.lib.maintainers.raskin ];
