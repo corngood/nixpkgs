aaf49265e5a8bf413d7de07f55335ec13a01b15b
David McFarland <corngood@gmail.com>
2020-09-17 18:11:31 -0300

    wip

--- pkgs/stdenv/cygwin/default.nix
+++ pkgs/stdenv/cygwin/default.nix
@@ -6,49 +6,51 @@
 
 bootStages ++ [
 
-  # (prevStage: {
-  #   inherit config overlays;
-  #   stdenv = with prevStage; import ../generic {
-  #     inherit (stdenv)
-  #       fetchurlBoot;
-  #     inherit
-  #       config
-  #       buildPlatform
-  #       hostPlatform
-  #       targetPlatform;
-  #     shell = "${bash}/bin/bash";
-  #     initialPath = import ../common-path.nix { pkgs = prevStage; };
-  #     cc = import ../../build-support/cc-wrapper {
-  #       nativeTools = false;
-  #       nativeLibc = false;
-  #       libc = cygwin.packages.cygwin-devel;
-  #       cc = callPackage ../../development/compilers/gcc/9 {
-  #         stdenv = stdenv // {
-  #           cc = stdenv.cc // {
-  #             libc = cygwin.packages.cygwin-devel;
-  #             w32api-headers = cygwin.packages.w32api-headers;
-  #             nativeLibc = false;
-  #           };
-  #         };
-  #         noSysDirs = true;
-  #         profiledCompiler = false;
-  #         libcCross = null;
-  #         isl = isl_0_17;
-  #       } // { hardeningUnsupportedFlags = [ "fortify"]; };
-  #       bintools = import ../../build-support/bintools-wrapper {
-  #         name = "bintools";
-  #         nativeTools = false;
-  #         nativeLibc = false;
-  #         libc = cygwin.packages.cygwin-devel;
-  #         bintools = binutils-unwrapped;
-  #         extraBuildCommands = ''
-  #           echo "-L${cygwin.packages.w32api-runtime}/lib/w32api" > $out/nix-support/libc-ldflags
-  #         '';
-  #         inherit stdenvNoCC coreutils gnugrep;
-  #       };
-  #       inherit stdenvNoCC coreutils gnugrep;
-  #     };
-  #   };
-  # })
+  (prevStage: {
+    inherit config overlays;
+    stdenv = with prevStage; import ../generic {
+      inherit (stdenv)
+        fetchurlBoot
+        shell
+        initialPath;
+      inherit
+        config
+        buildPlatform
+        hostPlatform
+        targetPlatform;
+      # shell = "${bash}/bin/bash";
+      # initialPath = import ../common-path.nix { pkgs = prevStage; };
+      cc = import ../../build-support/cc-wrapper {
+        nativeTools = false;
+        nativeLibc = false;
+        libc = cygwin.packages.cygwin-devel;
+        cc = callPackage ../../development/compilers/gcc/9 {
+          stdenv = stdenv // {
+            cc = stdenv.cc // {
+              libc = cygwin.packages.cygwin-devel;
+              w32api-headers = cygwin.packages.w32api-headers;
+              nativeLibc = false;
+            };
+          };
+          noSysDirs = true;
+          profiledCompiler = false;
+          libcCross = null;
+          isl = isl_0_17;
+        } // { hardeningUnsupportedFlags = [ "fortify"]; };
+        bintools = import ../../build-support/bintools-wrapper {
+          name = "bintools";
+          nativeTools = false;
+          nativeLibc = false;
+          libc = cygwin.packages.cygwin-devel;
+          bintools = binutils-unwrapped;
+          extraBuildCommands = ''
+            echo "-L${cygwin.packages.w32api-runtime}/lib/w32api" > $out/nix-support/libc-ldflags
+          '';
+          inherit stdenvNoCC coreutils gnugrep;
+        };
+        inherit stdenvNoCC coreutils gnugrep;
+      };
+    };
+  })
 
 ]
