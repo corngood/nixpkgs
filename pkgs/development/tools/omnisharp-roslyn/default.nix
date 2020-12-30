40d3b391737668708a851b3ef8f969e6d6ebb1b5
David McFarland <corngood@gmail.com>
2020-12-30 10:21:44 -0400

    omnisharp-roslyn: use links for internal msbuild

--- pkgs/development/tools/omnisharp-roslyn/default.nix
+++ pkgs/development/tools/omnisharp-roslyn/default.nix
@@ -1,8 +1,7 @@
 { lib, stdenv
 , fetchurl
-, mono
+, mono6
 , msbuild
-, dotnet-sdk
 , makeWrapper
 , dotnetPackages
 }:
@@ -17,7 +16,7 @@ stdenv.mkDerivation rec {
     sha256 = "0kgv4l15rli9a7grmcsbv72csmxi7vqa7lrrr8bd4cq9ighh54q3";
   };
 
-  nativeBuildInputs = [ makeWrapper dotnet-sdk dotnetPackages.Nuget ];
+  nativeBuildInputs = [ makeWrapper dotnetPackages.Nuget ];
 
   preUnpack = ''
     mkdir src
@@ -29,14 +28,16 @@ stdenv.mkDerivation rec {
     mkdir -p $out/bin
     cd ..
     cp -r src $out/
-    rm -r $out/src/.msbuild
-    cp -r ${msbuild}/lib/mono/msbuild $out/src/.msbuild
+    rm -rf $out/src/.msbuild
+    mkdir $out/src/.msbuild
+    ln -s ${msbuild}/lib/mono/xbuild/* $out/src/.msbuild/
+    rm $out/src/.msbuild/Current
+    mkdir $out/src/.msbuild/Current
+    ln -s ${msbuild}/lib/mono/xbuild/Current/* $out/src/.msbuild/Current/
+    ln -s ${msbuild}/lib/mono/msbuild/Current/bin $out/src/.msbuild/Current/Bin
 
-    chmod -R u+w $out/src
-    mv $out/src/.msbuild/Current/{bin,Bin}
-
-    makeWrapper ${mono}/bin/mono $out/bin/omnisharp \
-    --add-flags "$out/src/OmniSharp.exe"
+    makeWrapper ${mono6}/bin/mono $out/bin/omnisharp \
+      --add-flags "$out/src/OmniSharp.exe"
   '';
 
   meta = with lib; {
