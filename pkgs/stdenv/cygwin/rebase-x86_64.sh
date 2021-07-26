b08801ae62d4cccd553b29d843bd13c44a89aff4
David McFarland <corngood@gmail.com>
2021-07-26 15:41:12 -0300

    cygwin: rebase dlls based on output hash

--- pkgs/stdenv/cygwin/rebase-x86_64.sh
+++ pkgs/stdenv/cygwin/rebase-x86_64.sh
@@ -1,14 +1,20 @@
 fixupOutputHooks+=(_cygwinFixAutoImageBase)
 
+_cygwinGetImageBase() {
+    printf \
+        '0x%x\n' \
+        $(( \
+            $((0x$(echo "$1" | sha1sum | cut -c1-15) & 0x7fffffffffff0000)) \
+                % $((0x770000000000 - 0x400000000)) + 0x400000000 \
+         ))
+}
+
 _cygwinFixAutoImageBase() {
     if [ "${dontRebase-}" == 1 ] || [ ! -d "$prefix" ]; then
         return
     fi
     find "$prefix" -name "*.dll" -type f | while read DLL; do
-        if [ -f /etc/rebasenix.nextbase ]; then
-            NEXTBASE="$(</etc/rebasenix.nextbase)"
-        fi
-        NEXTBASE=${NEXTBASE:-0x200010000}
+        NEXTBASE=${NEXTBASE:-$(_cygwinGetImageBase "$prefix")}
 
         REBASE=(`/bin/rebase -i $DLL`)
         BASE=${REBASE[2]}
@@ -18,7 +24,5 @@ _cygwinFixAutoImageBase() {
         echo "REBASE FIX: $DLL $BASE -> $NEXTBASE"
         /bin/rebase -b $NEXTBASE $DLL
         NEXTBASE="0x`printf %x $(($NEXTBASE+$SKIP))`"
-
-        echo $NEXTBASE > /etc/rebasenix.nextbase
     done
 }
