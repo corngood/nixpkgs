14d02bef6495d6e7f4cf0d122f0f482e80cd363b
David McFarland <corngood@gmail.com>
2023-06-20 23:33:38 -0300

    omnisharp-roslyn: use source build of lsp package

---
+++ pkgs/development/libraries/csharp-language-server-protocol/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildDotnetModule
+, dotnetCorePackages
+, fetchFromGitHub
+}:
+let
+  dotnet-sdk = with dotnetCorePackages; combinePackages [ sdk_6_0 sdk_7_0 ];
+  dotnet-runtime = dotnetCorePackages.runtime_7_0;
+
+in buildDotnetModule rec {
+  inherit dotnet-sdk dotnet-runtime;
+
+  pname = "csharp-language-server-protocol";
+  version = "0.19.9";
+
+  src = fetchFromGitHub {
+    owner = "OmniSharp";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-NAzBm5ZaUtw+nJ4DhVofczM+k5p8Eil/lVNi+t2j4mU=";
+  };
+
+  patches = [
+    ./0001-wip-fix-blocking-on-stdin.patch
+  ];
+
+  enableParallelBuilding = false;
+
+  useAppHost = false;
+
+  dotnetPackFlags = [ "-p:PackageIcon=" ];
+
+  nugetDeps = ./deps.nix;
+
+  dontPublish = true;
+  packNupkg = true;
+}
