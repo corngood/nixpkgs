6c14ae931b1efbd4537669dd29a3eef685f5cfcf
David McFarland <corngood@gmail.com>
2023-07-15 13:39:26 -0300

    fixup! wip

--- pkgs/development/compilers/dotnet/build-dotnet.nix
+++ pkgs/development/compilers/dotnet/build-dotnet.nix
@@ -116,6 +116,7 @@ stdenv.mkDerivation (finalAttrs: rec {
     export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1 # Dont try to expand NuGetFallbackFolder to disk
     export DOTNET_NOLOGO=1 # Disables the welcome message
     export DOTNET_CLI_TELEMETRY_OPTOUT=1
+    export MSBUILDALWAYSOVERWRITEREADONLYFILES=1 # files copied from /nix/store
   '' + lib.optionalString (type == "sdk") ''
     if [ ! -w "$HOME" ]; then
       export HOME=$(mktemp -d) # Dotnet expects a writable home directory for its configuration files
