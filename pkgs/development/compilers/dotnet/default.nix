ed085af9179191e7835742ae4daa8a3d3026371f
David McFarland <corngood@gmail.com>
2024-03-05 18:41:32 -0400

    wip

--- pkgs/development/compilers/dotnet/default.nix
+++ pkgs/development/compilers/dotnet/default.nix
@@ -48,10 +48,36 @@ in
     mkNugetDeps,
     git,
     glibcLocales,
-    xmlstarlet
+    xmlstarlet,
+    fetchurl,
+    linkFarm
   }: stdenvNoCC.mkDerivation (finalAttrs:
   let
     sdk = dotnet_8_0_102.sdk_8_0;
+    bins = linkFarm "bins" (map ({name, hash}: {
+      name = name;
+      path = fetchurl {
+        url = "https://dotnetcli.blob.core.windows.net/dotnet/${name}";
+        inherit hash;
+      };
+    }) [
+      {
+        name = "Runtime/8.0.2-servicing.24067.11/dotnet-runtime-8.0.2-linux-x64.tar.gz";
+        hash = "sha256-5Oepaqyb0Uec9yjTCWXQ357Aoi/DXTRTjnuqz7WgdJE=";
+      }
+      {
+        name = "Sdk/8.0.200-rtm.24069.18/dotnet-toolset-internal-8.0.200-rtm.24069.18.zip";
+        hash = "sha256-sa+KmWOhPa6Mppp6mzJ3mp9E+tvnwSDJXHfE1Lmonww=";
+      }
+      {
+        name = "aspnetcore/Runtime/8.0.2-servicing.24068.4/aspnetcore-runtime-8.0.2-linux-x64.tar.gz";
+        hash = "sha256-l0ioxEsgIZt1Kbjd6MF6oH362wIGYJ6mznMpJpN5oSE=";
+      }
+      {
+        name = "aspnetcore/Runtime/8.0.2-servicing.24068.4/aspnetcore_base_runtime.version";
+        hash = "sha256-/TCSlKch6sJ3iTU6nyMGAGEhmnmn0Gukf60jpvWqARU=";
+      }
+    ]);
   in rec {
     name = "installer";
     version = "8.0.201";
@@ -129,7 +155,7 @@ in
     buildPhase = ''
       runHook preBuild
 
-      ./build.sh --build --configuration Release
+      ./build.sh --build --configuration Release -p:PublicBaseURL=file://${bins}/
 
       runHook postBuild
     '';
