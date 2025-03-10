b1b4848df8b4a35df210eaf943fc9ee168f73303
David McFarland <corngood@gmail.com>
2025-03-10 01:43:30 -0300

    WIP: godot: package export templates

--- pkgs/development/tools/godot/4.3/default.nix
+++ pkgs/development/tools/godot/4.3/default.nix
@@ -1,6 +1,11 @@
 {
   version = "4.3-stable";
   hash = "sha256-MzElflwXHWLgPtoOIhPLA00xX8eEdQsexZaGIEOzbj0=";
-  exportTemplatesHash = "sha256-9fENuvVqeQg0nmS5TqjCyTwswR+xAUyVZbaKK7Q3uSI=";
-  nugetDeps = ./deps.json;
+  default = {
+    exportTemplatesHash = "sha256-9fENuvVqeQg0nmS5TqjCyTwswR+xAUyVZbaKK7Q3uSI=";
+  };
+  mono = {
+    exportTemplatesHash = "sha256-9fENuvVqeQg0nmS5TqjCyTwswR+xAUyVZbaKK7Q0u000";
+    nugetDeps = ./deps.json;
+  };
 }
--- pkgs/development/tools/godot/4.4/default.nix
+++ pkgs/development/tools/godot/4.4/default.nix
@@ -1,6 +1,11 @@
 {
   version = "4.4.1-stable";
   hash = "sha256-O4TdPYu1K2zWKMBP/7xd0UPLDb7/4dBnkGM7QydD3Yo=";
-  exportTemplatesHash = "sha256-eo0UreSJ/U0i8XgZMCH+iodqnlEGjtTd4m2sOuTFmog=";
-  nugetDeps = ./deps.json;
+  default = {
+    exportTemplatesHash = "sha256-eo0UreSJ/U0i8XgZMCH+iodqnlEGjtTd4m2sOuTFmog=";
+  };
+  mono = {
+    exportTemplatesHash = "sha256-QFdwWrowa6Q9ruWdnXIkea7qTea3DlkEtD7QpyQ00000";
+    nugetDeps = ./deps.json;
+  };
 }
