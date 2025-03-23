e8df199897576814c6f6e3a5a15037a204c34256
David McFarland <corngood@gmail.com>
2025-03-23 00:47:25 -0300

    godot: disable strict aliasing

--- pkgs/development/tools/godot/common.nix
+++ pkgs/development/tools/godot/common.nix
@@ -156,6 +156,9 @@ let
 
       module_mono_enabled = withMono;
 
+      # aliasing bugs exist with hardening+LTO
+      # https://github.com/godotengine/godot/pull/104501
+      ccflags = "-fno-strict-aliasing";
       linkflags = "-Wl,--build-id";
 
       # libraries that aren't available in nixpkgs
