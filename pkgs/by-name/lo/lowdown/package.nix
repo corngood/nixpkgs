c16e038ad8409d18349fe220ef42315eabbab0cb
David McFarland <corngood@gmail.com>
2025-12-11 14:45:43 -0400

    treewide: undo changes causing rebuilds

--- pkgs/by-name/lo/lowdown/package.nix
+++ pkgs/by-name/lo/lowdown/package.nix
@@ -95,11 +95,11 @@ stdenv.mkDerivation rec {
     "install_static"
   ];
 
-  postInstall = lib.optionalString stdenv.hostPlatform.isCygwin ''
+  postInstall = if stdenv.hostPlatform.isCygwin then ''
     mkdir -p "$lib"/bin
     mv "$lib"/lib/*.dll "$lib"/bin/
     chmod +x "$lib"/bin/*.dll
-  '';
+  '' else null;
 
   doInstallCheck = !stdenv.hostPlatform.isDarwin || !enableDarwinSandbox;
   installCheckPhase = ''
