2e146cfd523c5a45776a73fd18f49e21bd2e2bc7
David McFarland <corngood@gmail.com>
2025-12-22 23:06:45 -0400

    WIP: cygwin bootstrap stdenv

--- pkgs/stdenv/cygwin/default.nix
+++ pkgs/stdenv/cygwin/default.nix
@@ -8,7 +8,13 @@
   bootstrapFiles ?
     let
       table = {
-        x86_64-cygwin = import ./bootstrap-files/x86_64-pc-cygwin.nix;
+        x86_64-cygwin = {
+          # import ./bootstrap-files/x86_64-pc-cygwin.nix;
+          unpack = lib.toDerivation /nix/store/2g6dzrj4xlv9rdnbynlflk7s4ymsqy91-unpacked // {
+            name = "unpacked";
+            version = "foo";
+          };
+        };
       };
       files =
         table.${localSystem.system}
@@ -19,18 +25,83 @@
 
 assert crossSystem == localSystem;
 let
-  bootStages = import ../. {
-    inherit lib overlays;
+  # bootStages = import ../. {
+  #   inherit lib overlays;
 
-    localSystem = lib.systems.elaborate "x86_64-linux";
+  #   localSystem = lib.systems.elaborate "x86_64-linux";
 
-    crossSystem = localSystem;
-    crossOverlays = [ ];
+  #   crossSystem = localSystem;
+  #   crossOverlays = [ ];
 
-    # Ignore custom stdenvs when cross compiling for compatibility
-    # Use replaceCrossStdenv instead.
-    config = builtins.removeAttrs config [ "replaceStdenv" ];
-  };
+  #   # Ignore custom stdenvs when cross compiling for compatibility
+  #   # Use replaceCrossStdenv instead.
+  #   config = builtins.removeAttrs config [ "replaceStdenv" ];
+  # };
+
+  bootStages = [ ];
+
+  linkBootstrap = (
+    attrs:
+    derivation (
+      attrs
+      // {
+        inherit (localSystem) system;
+        name = attrs.name or (baseNameOf (builtins.elemAt attrs.paths 0));
+        src = bootstrapFiles.unpack;
+        builder = "${bootstrapFiles.unpack}/bin/bash";
+        # this script will prefer to link files instead of copying them.
+        # this prevents clang in particular, but possibly others, from calling readlink(argv[0])
+        # and obtaining dependencies, ld(1) in particular, from there instead of $PATH.
+        args = [ ../freebsd/linkBootstrap.sh ];
+        PATH = "${bootstrapFiles.unpack}/bin";
+        paths = attrs.paths;
+      }
+    )
+  );
+
+  # bashNonInteractive = linkBootstrap {
+  #   paths = [
+  #     "bin/bash"
+  #     "bin/sh"
+  #   ];
+  #   shell = "bin/bash";
+  #   shellPath = "/bin/bash";
+  # };
+  # binutils-unwrapped = linkBootstrap {
+  #   name = "binutils";
+  #   paths = map (str: "bin/" + str) [
+  #     "ld"
+  #     #"as"
+  #     #"addr2line"
+  #     "ar"
+  #     #"c++filt"
+  #     #"elfedit"
+  #     #"gprof"
+  #     #"objdump"
+  #     "nm"
+  #     "objcopy"
+  #     "ranlib"
+  #     "readelf"
+  #     "size"
+  #     "strings"
+  #     "strip"
+  #   ];
+  # };
+  bashNonInteractive = bootstrapFiles.unpack;
+  binutils-unwrapped = bootstrapFiles.unpack;
+  cocom-tool-set = bootstrapFiles.unpack;
+  coreutils = bootstrapFiles.unpack;
+  curl = bootstrapFiles.unpack;
+  expand-response-params = bootstrapFiles.unpack;
+  # gcc-unwrapped = linkBootstrap {
+  #   name = "gcc";
+  #   paths = map (str: "bin/" + str) [
+  #     "gcc"
+  #     "g++"
+  #   ];
+  # };
+  gcc-unwrapped = bootstrapFiles.unpack;
+  gnugrep = bootstrapFiles.unpack;
 
 in
 bootStages
@@ -41,12 +112,13 @@ bootStages
     let
       name = "cygwin";
 
-      initialPath =
-        ((import ../generic/common-path.nix) { pkgs = prevStage; })
-        # needed for cygwin1.dll
-        ++ [ "/" ];
+      initialPath = [
+        bootstrapFiles.unpack
+      ]
+      # needed for cygwin1.dll
+      ++ [ "/" ];
 
-      shell = "${prevStage.bashNonInteractive}/bin/bash";
+      shell = "${bashNonInteractive}/bin/bash";
 
       stdenvNoCC = import ../generic {
         inherit
@@ -63,18 +135,9 @@ bootStages
       };
 
       fetchurlBoot = import ../../build-support/fetchurl {
-        inherit lib stdenvNoCC;
-        inherit (prevStage) curl;
+        inherit lib stdenvNoCC curl;
         inherit (config) rewriteURL hashedMirrors;
       };
-
-      gcc = (
-        prevStage.gccFun {
-          noSysDirs = true;
-          majorMinorVersion = toString prevStage.default-gcc-version;
-          targetPackages.stdenv.cc.bintools = prevStage.stdenv.cc.bintools;
-        }
-      );
 
     in
     {
@@ -92,53 +155,200 @@ bootStages
           shell
           ;
 
-        cc = lib.makeOverridable (import ../../build-support/cc-wrapper) {
-          inherit lib stdenvNoCC;
-          name = "${name}-cc";
-          cc = gcc;
-          isGNU = true;
-          libc = prevStage.cygwin.newlib-cygwin;
-          inherit (prevStage) gnugrep coreutils expand-response-params;
-          nativeTools = false;
-          nativeLibc = false;
-          propagateDoc = false;
-          runtimeShell = shell;
-          bintools = lib.makeOverridable (import ../../build-support/bintools-wrapper) {
-            inherit lib stdenvNoCC;
-            name = "${name}-bintools";
-            bintools = prevStage.bintools-unwrapped;
-            libc = prevStage.cygwin.newlib-cygwin;
-            inherit (prevStage) gnugrep coreutils expand-response-params;
-            nativeTools = false;
-            nativeLibc = false;
-            propagateDoc = false;
-            runtimeShell = shell;
-          };
-        };
+        hasCC = false;
+        cc = null;
 
         overrides = self: super: {
+          inherit cocom-tool-set;
           fetchurl = lib.makeOverridable fetchurlBoot;
-          __bootstrapPackages =
-            (import ../generic/common-path.nix) { pkgs = prevStage; }
-            ++ [
-              gcc
-              gcc.lib
-            ]
-            ++ (with prevStage; [
-              curl
-              curl.dev
-              cygwin.newlib-cygwin
-              cygwin.newlib-cygwin.bin
-              cygwin.newlib-cygwin.dev
-              cygwin.w32api
-              cygwin.w32api.dev
-              bintools-unwrapped
-              gnugrep
-              coreutils
-              expand-response-params
-            ]);
+          fetchgit = super.fetchgit.override {
+            git = bootstrapFiles.unpack;
+            cacert = null;
+            git-lfs = null;
+          };
         };
       };
     }
   )
+
+  (prevStage: {
+    inherit config overlays;
+    inherit (prevStage) stdenvNoCC;
+
+    stdenv = import ../generic rec {
+      name = "stdenv-cygwin";
+
+      buildPlatform = localSystem;
+      hostPlatform = localSystem;
+      targetPlatform = localSystem;
+      inherit
+        config
+        ;
+      inherit (prevStage.stdenv) fetchurlBoot initialPath shell;
+
+      cc = lib.makeOverridable (import ../../build-support/cc-wrapper) {
+        inherit lib;
+        inherit (prevStage) stdenvNoCC;
+        name = "${name}-cc";
+        cc = gcc-unwrapped;
+        isGNU = true;
+        libc = prevStage.cygwin.newlib-cygwin-headers;
+        inherit gnugrep coreutils;
+        expand-response-params = "";
+        nativeTools = false;
+        nativeLibc = false;
+        propagateDoc = false;
+        runtimeShell = shell;
+        bintools = lib.makeOverridable (import ../../build-support/bintools-wrapper) {
+          inherit lib;
+          inherit (prevStage) stdenvNoCC;
+          name = "${name}-bintools";
+          bintools = binutils-unwrapped;
+          libc = prevStage.cygwin.newlib-cygwin-headers;
+          inherit gnugrep coreutils;
+          expand-response-params = "";
+          nativeTools = false;
+          nativeLibc = false;
+          propagateDoc = false;
+          runtimeShell = shell;
+        };
+      };
+
+      overrides = self: super: {
+        fetchurl = lib.makeOverridable fetchurlBoot;
+        fetchgit = super.fetchgit.override {
+          git = bootstrapFiles.unpack;
+          cacert = null;
+          git-lfs = null;
+        };
+        # __bootstrapFiles = bootstrapFiles;
+        # __bootstrapPackages =
+        #   (import ../generic/common-path.nix) { pkgs = prevStage; }
+        #   ++ [
+        #     gcc
+        #     gcc.lib
+        #   ]
+        #   ++ (with prevStage; [
+        #     curl
+        #     curl.dev
+        #     cygwin.newlib-cygwin
+        #     cygwin.newlib-cygwin.bin
+        #     cygwin.newlib-cygwin.dev
+        #     cygwin.w32api
+        #     cygwin.w32api.dev
+        #     bintools-unwrapped
+        #     gnugrep
+        #     coreutils
+        #     expand-response-params
+        #   ]);
+      };
+    };
+  })
+
+  # (
+  #   prevStage:
+  #   let
+  #     name = "cygwin";
+
+  #     initialPath =
+  #       ((import ../generic/common-path.nix) { pkgs = prevStage; })
+  #       # needed for cygwin1.dll
+  #       ++ [ "/" ];
+
+  #     shell = "${prevStage.bashNonInteractive}/bin/bash";
+
+  #     stdenvNoCC = import ../generic {
+  #       inherit
+  #         config
+  #         initialPath
+  #         shell
+  #         fetchurlBoot
+  #         ;
+  #       name = "stdenvNoCC-${name}";
+  #       buildPlatform = localSystem;
+  #       hostPlatform = localSystem;
+  #       targetPlatform = localSystem;
+  #       cc = null;
+  #     };
+
+  #     fetchurlBoot = import ../../build-support/fetchurl {
+  #       inherit lib stdenvNoCC;
+  #       inherit (prevStage) curl;
+  #       inherit (config) rewriteURL hashedMirrors;
+  #     };
+
+  #     gcc = (
+  #       prevStage.gccFun {
+  #         noSysDirs = true;
+  #         majorMinorVersion = toString prevStage.default-gcc-version;
+  #         targetPackages.stdenv.cc.bintools = prevStage.stdenv.cc.bintools;
+  #       }
+  #     );
+
+  #   in
+  #   {
+  #     inherit config overlays stdenvNoCC;
+  #     stdenv = import ../generic rec {
+  #       name = "stdenv-cygwin";
+
+  #       buildPlatform = localSystem;
+  #       hostPlatform = localSystem;
+  #       targetPlatform = localSystem;
+  #       inherit
+  #         config
+  #         initialPath
+  #         fetchurlBoot
+  #         shell
+  #         ;
+
+  #       cc = lib.makeOverridable (import ../../build-support/cc-wrapper) {
+  #         inherit lib stdenvNoCC;
+  #         name = "${name}-cc";
+  #         cc = gcc;
+  #         isGNU = true;
+  #         libc = prevStage.cygwin.newlib-cygwin;
+  #         inherit (prevStage) gnugrep coreutils expand-response-params;
+  #         nativeTools = false;
+  #         nativeLibc = false;
+  #         propagateDoc = false;
+  #         runtimeShell = shell;
+  #         bintools = lib.makeOverridable (import ../../build-support/bintools-wrapper) {
+  #           inherit lib stdenvNoCC;
+  #           name = "${name}-bintools";
+  #           bintools = prevStage.bintools-unwrapped;
+  #           libc = prevStage.cygwin.newlib-cygwin;
+  #           inherit (prevStage) gnugrep coreutils expand-response-params;
+  #           nativeTools = false;
+  #           nativeLibc = false;
+  #           propagateDoc = false;
+  #           runtimeShell = shell;
+  #         };
+  #       };
+
+  #       overrides = self: super: {
+  #         fetchurl = lib.makeOverridable fetchurlBoot;
+  #         __bootstrapFiles = bootstrapFiles;
+  #         __bootstrapPackages =
+  #           (import ../generic/common-path.nix) { pkgs = prevStage; }
+  #           ++ [
+  #             gcc
+  #             gcc.lib
+  #           ]
+  #           ++ (with prevStage; [
+  #             curl
+  #             curl.dev
+  #             cygwin.newlib-cygwin
+  #             cygwin.newlib-cygwin.bin
+  #             cygwin.newlib-cygwin.dev
+  #             cygwin.w32api
+  #             cygwin.w32api.dev
+  #             bintools-unwrapped
+  #             gnugrep
+  #             coreutils
+  #             expand-response-params
+  #           ]);
+  #       };
+  #     };
+  #   }
+  # )
 ]
--- pkgs/stdenv/cygwin/make-bootstrap-tools.nix
+++ pkgs/stdenv/cygwin/make-bootstrap-tools.nix
@@ -28,7 +28,7 @@ let
         let
         in
         ''
-          rsync --chmod="+w" -av -L --exclude=cygwin1.dll "${pack-env pkgs}"/ .
+          rsync --chmod="+w" -av --safe-links --exclude=cygwin1.dll "${pack-env pkgs}"/ .
 
           base=$PWD
           rm -rf nix nix-support
@@ -64,13 +64,82 @@ let
       rm "$dev"/bin/curl-config "$bin"/bin/wcurl
     '';
   });
+
+  inherit (pkgs)
+    bashNonInteractive
+    binutils-unwrapped
+    bzip2
+    coreutils
+    findutils
+    gawk
+    gcc-unwrapped
+    gitMinimal # for fetchgit (newlib-cygwin)
+    gnugrep
+    gnumake
+    gnused
+    gnutar
+    gzip
+    patch
+    ;
+
 in
 rec {
+  bootstrap-env = pack-env (
+    with pkgs;
+    [
+      bashNonInteractive
+      binutils-unwrapped
+      coreutils
+      gnugrep
+      curl
+      gcc-unwrapped
+      gitMinimal # for fetchgit (newlib-cygwin)
+      findutils
+      patch
+    ]
+  );
+
+  unpacked =
+    runCommand "unpacked"
+      {
+        nativeBuildInputs = [ rsync ];
+        # The result should not contain any references (store paths) so
+        # that we can safely copy them out of the store and to other
+        # locations in the store.
+        # TODO:
+        # allowedReferences = [ ];
+      }
+      ''
+        mkdir -p "$out"/{bin,include,lib,libexec}
+        cp -d "${bashNonInteractive}"/bin/* "$out"/bin/
+        cp -d "${binutils-unwrapped}"/bin/* "$out"/bin/
+        cp -d "${bzip2}"/bin/* "$out"/bin/
+        cp -d "${coreutils}"/bin/* "$out"/bin/
+        cp -d "${curl}"/bin/* "$out"/bin/
+        cp -d "${findutils}"/bin/* "$out"/bin/
+        cp -d "${gawk}"/bin/* "$out"/bin/
+        cp -d "${gcc-unwrapped}"/bin/* "$out"/bin/
+        cp -rd ${gcc-unwrapped}/include/* $out/include/
+        cp -rd ${gcc-unwrapped}/lib/* $out/lib/
+        cp -rd ${gcc-unwrapped}/libexec/* $out/libexec/
+        cp -d "${gitMinimal}"/bin/* "$out"/bin/
+        cp -d "${gnugrep}"/bin/* "$out"/bin/
+        cp -d "${gnumake}"/bin/* "$out"/bin/
+        cp -d "${gnused}"/bin/* "$out"/bin/
+        cp -d "${gnutar}"/bin/* "$out"/bin/
+        cp -d "${gzip}"/bin/* "$out"/bin/
+        cp -d "${patch}"/bin/* "$out"/bin/
+
+        for x in "$out"/bin/* "$out"/libexec/*/*/*; do
+          [[ -L "$x" && -e "$x" ]] || continue
+          [[ $(realpath "$x") != "$out"* ]] || continue
+          cp "$x" "$x"~
+          mv "$x"~ "$x"
+        done
+      '';
+
   unpack = nar-all "unpack.nar.xz" (with pkgs; [
     bashNonInteractive
-    mkdir
-    xz
-    gnutar
   ]) "";
   bootstrap-tools = tar-all "bootstrap-tools.tar.xz" (with pkgs; [
     gcc
@@ -90,6 +159,5 @@ rec {
   build = runCommand "build" { } ''
     mkdir -p $out/on-server
     ln -s ${unpack} $out/on-server/unpack.nar.xz
-    ln -s ${bootstrap-tools} $out/on-server/bootstrap-tools.tar.xz
   '';
 }
