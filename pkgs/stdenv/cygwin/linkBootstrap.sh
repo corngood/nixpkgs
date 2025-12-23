42a2f121e1ff9d74c804f07a28f44d8d69b21ae1
David McFarland <corngood@gmail.com>
2025-12-23 10:30:37 -0400

    fixup! WIP: cygwin bootstrap stdenv

--- pkgs/stdenv/cygwin/linkBootstrap.sh
+++ pkgs/stdenv/cygwin/linkBootstrap.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 for path in $paths; do
-    if [[ ! -e $src/$path ]]; then
-      path=$path.exe
-    fi
-    if ! [ -e "$src/$path" ]; then
-        echo "Error: $path does not exist"
-        exit 1
-    fi
-    mkdir -p $out/$(dirname $path)
-    ln -s $src/$path $out/$path
+  if [[ -e $src/$path.exe ]]; then
+    path=$path.exe
+  fi
+  if ! [[ -e $src/$path ]]; then
+    echo "Error: $path does not exist"
+    exit 1
+  fi
+  mkdir -p $out/$(dirname $path)
+  ln -s $src/$path $out/$path
 done
